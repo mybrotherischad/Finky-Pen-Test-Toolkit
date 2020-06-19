36dddd5d9b37cf2f97cef4bdccd2a957fe457675
Eric Finkenbiner <mybrotherischad@gmail.com>
2020-06-18 23:42:03 -0400

    Update nmap_scans.sh
    Fixed previously messed up commit (reverted before this)

--- nmap_scans.sh
+++ nmap_scans.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-# Don't forget to chmod +x the file
 echo "Enter the IP and/or range for initial scan: "
 read ips
 echo "-----------------------"
 echo "Working.........."
-nmap $ips -oA nmap-initial | grep "Nmap scan report for" | cut -d " " -f5 > ip_list.txt
+FIRSTDIR='nmap-initial'
+mkdir "$FIRSTDIR"
+nmap $ips -oA "$FIRSTDIR/nmap-initial" | grep "Nmap scan report for" | cut -d " " -f5 > ip_list.txt
 cat ip_list.txt
 read -p "Proceed to second level scan? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-        echo "Working.........."
-        sudo nmap -iL ip_list.txt -sC -sV -O -oA nmap-scsvo
-        read -p "Proceed to final TCP scan?? " -n 1 -r
-        echo    
-        if [[ $REPLY =~ ^[Yy]$ ]]
-        then
-                echo "Working.........."
-                sudo nmap -iL ip_list.txt -sC -sV -O -p- -oA nmap-fulltcp
-        fi
-
+	echo "Working.........."
+	SECDIR='nmap-scsvo'
+	mkdir -p "$SECDIR"
+	sudo nmap -iL ip_list.txt -sC -sV -O -oA "$SECDIR/nmap-scsvo"
+	read -p "Proceed to final TCP scan?? " -n 1 -r
+	echo    
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		FULLDIR='nmap-fulltcp'
+		mkdir -p "$FULLDIR"
+		echo "Working.........."
+		sudo nmap -iL ip_list.txt -sC -sV -O -p- -oA "$FULLDIR/nmap-fulltcp"
+	fi
+	
 fi
