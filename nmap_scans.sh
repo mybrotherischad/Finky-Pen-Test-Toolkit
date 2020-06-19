8a1924e612e93d8b9a1caf40f2385576c56d6c81
Eric Finkenbiner <mybrotherischad@gmail.com>
2020-06-18 23:38:41 -0400

    Revert "Update nmap_scans.sh"
    This reverts commit ac406add1db990271c7a1838b82bb9a5c1114a81.

--- nmap_scans.sh
+++ nmap_scans.sh
@@ -1,27 +1,23 @@
-ho "Enter the IP and/or range for initial scan: "
+#!/bin/bash
+# Don't forget to chmod +x the file
+echo "Enter the IP and/or range for initial scan: "
 read ips
 echo "-----------------------"
 echo "Working.........."
-FIRSTDIR='nmap-initial'
-mkdir "$FIRSTDIR"
-nmap $ips -oA "$FIRSTDIR/nmap-initial" | grep "Nmap scan report for" | cut -d " " -f5 > ip_list.txt
+nmap $ips -oA nmap-initial | grep "Nmap scan report for" | cut -d " " -f5 > ip_list.txt
 cat ip_list.txt
 read -p "Proceed to second level scan? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	echo "Working.........."
-	SECDIR='nmap-scsvo'
-	mkdir -p "$SECDIR"
-	sudo nmap -iL ip_list.txt -sC -sV -O -oA "$SECDIR/nmap-scsvo"
-	read -p "Proceed to final TCP scan?? " -n 1 -r
-	echo    
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		FULLDIR='nmap-fulltcp'
-		mkdir -p "$FULLDIR"
-		echo "Working.........."
-		sudo nmap -iL ip_list.txt -sC -sV -O -p- -oA "$FULLDIR/nmap-fulltcp"
-	fi
-	
+        echo "Working.........."
+        sudo nmap -iL ip_list.txt -sC -sV -O -oA nmap-scsvo
+        read -p "Proceed to final TCP scan?? " -n 1 -r
+        echo    
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+                echo "Working.........."
+                sudo nmap -iL ip_list.txt -sC -sV -O -p- -oA nmap-fulltcp
+        fi
+
 fi
