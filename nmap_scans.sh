f2171964d91cf4f94e9b9320e9657631617cba72
Eric Finkenbiner <mybrotherischad@gmail.com>
2020-06-28 15:52:46 -0400

    Removed Y/N prompts for deeper level scans. They will just proceed once password is entered for stage 2. Also, all output will be sent to top level folder named with the date/timestamp.

--- nmap_scans.sh
+++ nmap_scans.sh
@@ -3,26 +3,28 @@ echo "Enter the IP and/or range for initial scan: "
 read ips
 echo "-----------------------"
 echo "Working.........."
+TOPDIR=$(date +%Y%m%d%H%M%S)
 FIRSTDIR='nmap-initial'
-mkdir "$FIRSTDIR"
-nmap $ips -oA "$FIRSTDIR/nmap-initial" | grep "Nmap scan report for" | cut -d " " -f5 > ip_list.txt
-cat ip_list.txt
-read -p "Proceed to second level scan? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	echo "Working.........."
+mkdir "$TOPDIR"
+mkdir "$TOPDIR/$FIRSTDIR"
+nmap $ips -oA "$TOPDIR/$FIRSTDIR/nmap-initial" | grep "Nmap scan report for" | cut -d " " -f5 > $TOPDIR/ip_list.txt
+cat $TOPDIR/ip_list.txt
+#read -p "Proceed to second level scan? " -n 1 -r
+#echo
+#if [[ $REPLY =~ ^[Yy]$ ]]
+#then
+	echo "Working on second level scan.........."
 	SECDIR='nmap-scsvo'
-	mkdir -p "$SECDIR"
-	sudo nmap -iL ip_list.txt -sC -sV -O -oA "$SECDIR/nmap-scsvo"
-	read -p "Proceed to final TCP scan?? " -n 1 -r
-	echo    
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
+	mkdir -p "$TOPDIR/$SECDIR"
+	sudo nmap -iL $TOPDIR/ip_list.txt -sC -sV -O -oA "$TOPDIR/$SECDIR/nmap-scsvo"
+	#read -p "Proceed to final TCP scan?? " -n 1 -r
+	#echo    
+	#if [[ $REPLY =~ ^[Yy]$ ]]
+	#then
 		FULLDIR='nmap-fulltcp'
-		mkdir -p "$FULLDIR"
-		echo "Working.........."
-		sudo nmap -iL ip_list.txt -sC -sV -O -p- -oA "$FULLDIR/nmap-fulltcp"
-	fi
+		mkdir -p "$TOPDIR/$FULLDIR"
+		echo "Working on final level scans.........."
+		sudo nmap -iL $TOPDIR/ip_list.txt -sC -sV -O -p- -oA "$TOPDIR/$FULLDIR/nmap-fulltcp"
+	#fi
 	
-fi
+#fi
